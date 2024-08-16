c906975114370854738e6c4e8a7334455761e92a
danceq <140273750+danceqqq@users.noreply.github.com>
2024-08-17 00:36:08 +0300

    1.2
    Part of the bot :
    - The logic of sending an order has been changed
    - Added button design
    - Added an Order Search button
    
    
    Part of the application :
    - Another tkinter library is used
    
    - The position of the customer's photo has been changed
    
    - Added a button for contacting the customer
    
    - Minor performance fix

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-import tkinter as tk
+import customtkinter as ctk
+from tkinter import Listbox, Text, END
 from PIL import Image, ImageTk
 import sqlite3
 import os
+import webbrowser
+import requests
 
-
-class App(tk.Tk):
+class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Заказы")
-        self.geometry("800x600")
+        self.geometry("1000x800")
         self.conn = sqlite3.connect('orders.db')
         self.cursor = self.conn.cursor()
         self.cursor.execute('''
@@ -25,52 +27,85 @@ class App(tk.Tk):
         self.conn.commit()
 
         # Toolbar frame
-        toolbar = tk.Frame(self)
-        toolbar.pack(side=tk.TOP, fill=tk.X)
+        toolbar = ctk.CTkFrame(self)
+        toolbar.pack(side=ctk.TOP, fill=ctk.X)
+
+        # Search entry for order numbers
+        self.search_entry = ctk.CTkEntry(toolbar, width=200, placeholder_text="Поиск 🔎")
+        self.search_entry.pack(side=ctk.LEFT, padx=5, pady=5)
+        self.search_entry.bind('<FocusIn>', self.on_search_focus_in)
+        self.search_entry.bind('<FocusOut>', self.on_search_focus_out)
+        self.search_entry.bind('<KeyRelease>', self.on_search_key_release)
 
         # Buttons in the toolbar
-        self.update_button = tk.Button(toolbar, text="Обновить 🔄", command=self.update_orders)
-        self.update_button.pack(side=tk.LEFT, padx=5, pady=5)
-        self.accept_button = tk.Button(toolbar, text="Принять ✅", command=self.accept_order)
-        self.accept_button.pack(side=tk.LEFT, padx=5, pady=5)
-        self.cancel_button = tk.Button(toolbar, text="Отклонить ❌", command=self.cancel_order)
-        self.cancel_button.pack(side=tk.LEFT, padx=5, pady=5)
-        self.delete_button = tk.Button(toolbar, text="Удалить 🗑", command=self.delete_order)
-        self.delete_button.pack(side=tk.LEFT, padx=5, pady=5)
-        self.settings_button = tk.Button(toolbar, text="Настройки ⚙️", command=self.open_settings_window)
-        self.settings_button.pack(side=tk.RIGHT, padx=5, pady=5)
+        self.update_button = ctk.CTkButton(toolbar, text="Обновить 🔄", command=self.update_orders)
+        self.update_button.pack(side=ctk.LEFT, padx=5, pady=5)
+        self.accept_button = ctk.CTkButton(toolbar, text="Принять ✅", command=self.accept_order)
+        self.accept_button.pack(side=ctk.LEFT, padx=5, pady=5)
+        self.cancel_button = ctk.CTkButton(toolbar, text="Отклонить ❌", command=self.cancel_order)
+        self.cancel_button.pack(side=ctk.LEFT, padx=5, pady=5)
+        self.delete_button = ctk.CTkButton(toolbar, text="Удалить 🗑", command=self.delete_order)
+        self.delete_button.pack(side=ctk.LEFT, padx=5, pady=5)
+        self.settings_button = ctk.CTkButton(toolbar, text="Настройки ⚙️", command=self.open_settings_window)
+        self.settings_button.pack(side=ctk.RIGHT, padx=5, pady=5)
 
         # Main content
-        self.order_list = tk.Listbox(self, width=30)
-        self.order_list.pack(side=tk.LEFT, fill=tk.Y)
-        self.order_info = tk.Text(self, width=50, height=20)
-        self.order_info.pack(side=tk.TOP, fill=tk.BOTH)
+        self.order_list = Listbox(self, width=50, height=20)
+        self.order_list.pack(side=ctk.LEFT, fill=ctk.Y)
 
-        # Image label
-        self.image_label = tk.Label(self)
-        self.image_label.pack(side=tk.RIGHT, anchor="n", padx=10, pady=10)
+        self.order_frame = ctk.CTkFrame(self)
+        self.order_frame.pack(side=ctk.TOP, fill=ctk.BOTH, expand=True)
+
+        # Avatar and dialog button frame
+        self.avatar_frame = ctk.CTkFrame(self.order_frame)
+        self.avatar_frame.pack(side=ctk.LEFT, anchor="n", padx=10, pady=10)
+
+        # Label for Telegram avatar
+        self.avatar_label = ctk.CTkLabel(self.avatar_frame, text="")
+        self.avatar_label.pack(side=ctk.TOP)
+
+        # Button for opening Telegram dialog (initially hidden)
+        self.open_dialog_button = ctk.CTkButton(self.avatar_frame, text="Открыть диалог 📨", command=self.open_telegram_dialog)
+        self.open_dialog_button.pack(side=ctk.TOP, pady=10)
+
+        self.order_info = ctk.CTkTextbox(self.order_frame, width=500, height=300)
+        self.order_info.pack(side=ctk.LEFT, fill=ctk.BOTH)
+
+        # Image label for order image
+        self.image_label = ctk.CTkLabel(self, text="")
+        self.image_label.pack(side=ctk.RIGHT, anchor="n", padx=10, pady=10)
 
         self.update_orders()
         self.order_list.bind("<<ListboxSelect>>", lambda event: self.show_order_info())
 
-    def update_orders(self):
-        self.order_list.delete(0, tk.END)
-        self.cursor.execute('SELECT * FROM orders')
+        self.current_user_name = None
+
+    def on_search_focus_in(self, event):
+        if self.search_entry.get() == 'Поиск 🔎':
+            self.search_entry.delete(0, ctk.END)
+            self.search_entry.config(fg='black')
+
+    def on_search_focus_out(self, event):
+        if self.search_entry.get() == '':
+            self.search_entry.insert(0, 'Поиск 🔎')
+            self.search_entry.config(fg='gray')
+
+    def on_search_key_release(self, event):
+        search_text = self.search_entry.get().strip()
+        self.update_orders(search_text)
+
+    def update_orders(self, search_text=''):
+        self.order_list.delete(0, ctk.END)
+        query = 'SELECT * FROM orders'
+        params = ()
+        if search_text and search_text != 'Поиск 🔎':
+            query += ' WHERE order_number LIKE ?'
+            params = (f'%{search_text}%',)
+        self.cursor.execute(query, params)
         orders = self.cursor.fetchall()
         for order in orders:
             display_text = f"{order[4]} - {order[2]}"
-            self.order_list.insert(tk.END, display_text)
-
-            # Set color based on status
-            status = order[5]
-            if status == "New":
-                self.order_list.itemconfig(tk.END, {'fg': 'blue'})
-            elif status == "Cancel":
-                self.order_list.itemconfig(tk.END, {'fg': 'red'})
-            elif status == "Working":
-                self.order_list.itemconfig(tk.END, {'fg': 'green'})
-            elif status == "Deleted":
-                self.order_list.itemconfig(tk.END, {'fg': 'gray'})
+            self.order_list.insert(ctk.END, display_text)
 
     def accept_order(self):
         selected_order = self.order_list.curselection()
@@ -106,60 +141,87 @@ class App(tk.Tk):
             order_number = order_number.split(' - ')[0]
             self.cursor.execute('SELECT * FROM orders WHERE order_number =?', (order_number,))
             order = self.cursor.fetchone()
-            self.order_info.delete(1.0, tk.END)
-            self.order_info.insert(tk.END,
+            self.order_info.delete(1.0, ctk.END)
+            self.order_info.insert(ctk.END,
                                    f"Номер заказа: {order[4]}\n\nТема: {order[2]}\n\nОписание: {order[3]}\n\nПользователь: {order[1]}\n\nСтатус: {order[5]}")
 
-            # Проверка и отображение изображения, если оно существует
+            self.current_user_name = order[1]
+
+            # Check and display image if it exists
             user_folder = os.path.join('orders', order[1])
             image_path = os.path.join(user_folder, order[6]) if order[6] else None
 
             if image_path and os.path.exists(image_path):
                 image = Image.open(image_path)
-                image = self.resize_image(image, 300, 400)  # Устанавливаем размеры для масштабирования
+                image = self.resize_image(image, 300, 400)
                 photo = ImageTk.PhotoImage(image)
                 self.image_label.configure(image=photo)
-                self.image_label.image = photo  # Нужно сохранить ссылку на изображение, чтобы оно не было уничтожено
-                self.order_info.insert(tk.END, f"\n\n[Изображение прикреплено]\n")
+                self.image_label.image = photo
+                self.order_info.insert(ctk.END, f"\n\n[Изображение прикреплено]\n")
             else:
                 self.image_label.configure(image=None)
                 self.image_label.image = None
-                self.order_info.insert(tk.END, f"\n\n[Изображение отсутствует]\n")
+                self.order_info.insert(ctk.END, f"\n\n[Изображение отсутствует]\n")
+
+            self.load_and_display_avatar()
+            self.open_dialog_button.pack(side=ctk.TOP, pady=10)
 
     def resize_image(self, image, max_width, max_height):
-        """ Масштабирует изображение до заданных размеров, сохраняя пропорции """
         width, height = image.size
         ratio = min(max_width / width, max_height / height)
         new_size = (int(width * ratio), int(height * ratio))
         return image.resize(new_size, Image.Resampling.LANCZOS)
 
-    def open_settings_window(self):
-        settings_window = tk.Toplevel(self)
-        settings_window.title("Настройки")
-        settings_window.geometry("400x200")
+    def load_and_display_avatar(self):
+        avatar_url = f"https://t.me/i/userpic/320/{self.current_user_name}.jpg"
+        try:
+            response = requests.get(avatar_url, stream=True)
+            if response.status_code == 200:
+                avatar_image = Image.open(response.raw)
+                avatar_image = self.resize_image(avatar_image, 100, 100)
+                avatar_photo = ImageTk.PhotoImage(avatar_image)
+                self.avatar_label.configure(image=avatar_photo, text="")
+                self.avatar_label.image = avatar_photo
+            else:
+                self.avatar_label.configure(image=None, text="Аватар отсутствует")
+                self.avatar_label.image = None
+        except requests.RequestException as e:
+            self.avatar_label.configure(image=None, text="Ошибка загрузки аватара")
+            self.avatar_label.image = None
+            print(f"Ошибка загрузки аватара: {e}")
 
-        # Toolbar in the settings window
-        settings_toolbar = tk.Frame(settings_window)
-        settings_toolbar.pack(side=tk.TOP, fill=tk.X)
-        back_button = tk.Button(settings_toolbar, text="Вернуться ↩️", command=settings_window.destroy)
-        back_button.pack(side=tk.LEFT, padx=5, pady=5)
-
-        # Theme switcher
-        self.theme_var = tk.StringVar(value="light")
-        theme_switcher = tk.Checkbutton(settings_window, text="Тёмная тема", variable=self.theme_var, onvalue="dark",
-                                        offvalue="light", command=self.switch_theme)
-        theme_switcher.pack(pady=20)
-
-    def switch_theme(self):
-        if self.theme_var.get() == "dark":
-            self.configure(bg="#343434")
-            self.order_list.configure(bg="#343434", fg="white")
-            self.order_info.configure(bg="#343434", fg="white")
+    def open_telegram_dialog(self):
+        if self.current_user_name:
+            telegram_url = f"https://t.me/{self.current_user_name}"
+            webbrowser.open(telegram_url)
         else:
-            self.configure(bg="white")
-            self.order_list.configure(bg="white", fg="black")
-            self.order_info.configure(bg="white", fg="black")
+            print("Пользователь не выбран.")
 
+    def open_settings_window(self):
+        # Создаем новое окно настроек
+        settings_window = ctk.CTkToplevel(self)
+        settings_window.title("Настройки")
+        settings_window.geometry("300x200")
+
+        # Переключатель для смены темы
+        self.theme_switch = ctk.CTkSwitch(settings_window, text="Светлая тема", command=self.toggle_theme)
+        self.theme_switch.pack(pady=20)
+
+        # Устанавливаем переключатель в соответствии с текущей темой
+        if ctk.get_appearance_mode() == "Light":
+            self.theme_switch.select()
+        else:
+            self.theme_switch.deselect()
+
+        close_button = ctk.CTkButton(settings_window, text="Закрыть", command=settings_window.destroy)
+        close_button.pack(pady=20)
+
+    def toggle_theme(self):
+        # Смена темы между светлой и тёмной
+        if self.theme_switch.get():
+            ctk.set_appearance_mode("Light")
+        else:
+            ctk.set_appearance_mode("Dark")
 
 if __name__ == "__main__":
     app = App()
--- bot.py
+++ bot.py
@@ -1,89 +1,29 @@
 import logging
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 import sqlite3
+import random
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = '7415395822:AAFbLJzeX9Q81zsCq76V_Nh0J-AEJgjhv2o'
 
-
-async def start(update, context):
-    keyboard = [
-        [{"text": "Правила оформления", "callback_data": "rules"}],
-        [{"text": "Оформить заказ", "callback_data": "order"}],
-        [{"text": "Помощь", "callback_data": "help"}]
-    ]
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
-Привет! Я Тестовый бот для помощи в оформлении заказа.
-Настоятельно рекомендую перед заказом ознакомиться с правилом оформления заказа, ведь если вы не правильно заполните форму заказа, он не дойдёт до приложения!
-Используйте контекстные кнопки ниже, Для навигации :
-''', reply_markup={"inline_keyboard": keyboard})
-
-
-async def rules(update, context):
-    keyboard = [
-        [{"text": "Вернуться в начало", "callback_data": "start"}]
-    ]
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
-ᴛᴇᴋᴄᴛ нужно оɸоᴩʍᴧяᴛь ᴄᴛᴩоᴦо ᴨо ᴨᴩиʍᴇᴩу ᴄнизу.ᐟ
-⌞ ᴨᴇᴩʙᴀя ᴄᴛᴩоᴋᴀ⌝
-    ╰┈➤ᴛᴇʍᴀ зᴀᴋᴀзᴀ
-⌞ ʙᴛоᴩᴀя и ᴨоᴄᴧᴇдующиᴇ ᴄᴛᴩоᴋи⌝
-    ╰┈➤оᴨиᴄᴀниᴇ зᴀᴋᴀзᴀ
-⌞ ʙᴧожᴇния⌝
-   ╰┈➤ɸоᴛоᴦᴩᴀɸии  ✅
-   ╰┈➤ᴦоᴧоᴄоʙыᴇ ❌
-''', reply_markup={"inline_keyboard": keyboard})
-
-
-async def order(update, context):
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text='Пожалуйста, отправьте сообщение с описанием заказа в формате:\nТема заказа\nОписание заказа\nВы можете прикрепить фото к сообщению.')
-
-
-async def help(update, context):
-    keyboard = [
-        [{"text": "Вернуться в начало", "callback_data": "start"}]
-    ]
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
-ᴨо ᴛᴇхничᴇᴄᴋиʍ ʙоᴨᴩоᴮᴀʍ
-@ᴅᴀɴᴄᴇǫǫ
-ᴨо ʙоᴨᴩоᴮᴀʍ зᴀᴋᴀзоʙ 
-@ɴᴇsʜᴜᴍɪɪ
-''', reply_markup={"inline_keyboard": keyboard})
-
-
-async def button(update, context):
-    query = update.callback_query
-    await query.answer()
-    if query.data == "rules":
-        await rules(update, context)
-    elif query.data == "order":
-        await order(update, context)
-    elif query.data == "help":
-        await help(update, context)
-    elif query.data == "start":
-        await start(update, context)
-
-
+# Функция для создания таблицы в базе данных
 def ensure_table_exists():
     conn = sqlite3.connect('orders.db')
     cursor = conn.cursor()
 
-    # Создаем таблицу orders, если ее нет
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS orders (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_name TEXT,
             theme TEXT,
             description TEXT,
-            order_number INTEGER
+            order_number INTEGER,
+            status TEXT DEFAULT 'Working'  -- Добавляем столбец для состояния заказа
         )
     ''')
 
-    # Проверяем, есть ли столбец image, и добавляем его, если нет
     cursor.execute("PRAGMA table_info(orders)")
     columns = [column[1] for column in cursor.fetchall()]
 
@@ -94,76 +34,166 @@ def ensure_table_exists():
     conn.close()
 
 
-async def handle_message(update, context):
-    ensure_table_exists()
-    text = update.message.text
-    user_name = update.message.from_user.username
+# Функция для начала работы с ботом
+async def start(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Правила оформления 📖", callback_data="rules")],
+        [InlineKeyboardButton("Оформить заказ 🛒", callback_data="order")],
+        [InlineKeyboardButton("Помощь ❓", callback_data="help")],
+        [InlineKeyboardButton("Поиск по номеру 🔎", callback_data="search")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
+Привет! Я Тестовый бот для помощи в оформлении заказа.
+Настоятельно рекомендую перед заказом ознакомиться с правилом оформления заказа, ведь если вы не правильно заполните форму заказа, он не дойдёт до приложения!
+Используйте контекстные кнопки ниже для навигации:
+''', reply_markup=reply_markup)
+
+
+# Функция, выводящая правила оформления заказа
+async def rules(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Вернуться в начало", callback_data="start")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
+ᴛᴇᴋᴄᴛ нужно оɸоᴩʍᴧяᴛь ᴄᴛᴩоᴦо ᴨо ᴨᴩиʍᴇᴩу ᴄнизу.ᐟ
+⌞ ᴨᴇᴩʙᴀя ᴄᴛᴩоᴋᴀ⌝
+    ╰┈➤ᴛᴇʍᴀ зᴀᴋᴀзᴀ
+⌞ ʙᴛоᴩᴀя и ᴨоᴄᴧᴇдующиᴇ ᴄᴛᴩоᴋи⌝
+    ╰┈➤оᴨиᴄᴀниᴇ зᴀᴋᴀзᴀ
+⌞ ʙᴧожᴇния⌝
+   ╰┈➤ɸоᴛоᴦᴩᴀɸии  ✅
+   ╰┈➤ᴦоᴧоᴄоʙыᴇ ❌
+''', reply_markup=reply_markup)
+
+
+# Функция для оформления заказа
+async def order(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Вернуться", callback_data="start")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text='Пожалуйста, отправьте сообщение с описанием заказа в формате:\nТема заказа\nОписание заказа\nВы можете прикрепить фото к сообщению.',
+                                   reply_markup=reply_markup)
+    context.user_data['can_submit_order'] = True  # Разрешаем отправку заказа
+
+
+# Функция помощи
+async def help(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Вернуться в начало", callback_data="start")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='''
+ᴨо ᴛᴇхничᴇᴄᴋиʍ ʙоᴨᴩоᴮᴀʍ
+@ᴅᴀɴᴄᴇǫǫ
+ᴨо ʙоᴨᴩоᴄᴀʍ зᴀᴋᴀзоʙ 
+@ɴᴇsʜᴜᴍɪɪ
+''', reply_markup=reply_markup)
+
+
+# Функция для поиска заказа по номеру
+async def search_order(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Вернуться", callback_data="start")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text='Пожалуйста, введите номер заказа, который вы хотите найти:',
+                                   reply_markup=reply_markup)
+    context.user_data['waiting_for_order_number'] = True  # Устанавливаем состояние для ожидания ввода номера заказа
+
+
+# Функция обработки номера заказа
+async def handle_order_number(update, context):
+    order_number = update.message.text
     conn = sqlite3.connect('orders.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT MAX(order_number) FROM orders')
-    max_order_number = cursor.fetchone()[0]
-    if max_order_number is None:
-        order_number = 1
+
+    cursor.execute('SELECT user_name, theme, description, status FROM orders WHERE order_number = ?', (order_number,))
+    order = cursor.fetchone()
+
+    if order:
+        user_name, theme, description, status = order
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f'Заказ №{order_number}\n'
+                                            f'Пользователь: @{user_name}\n'
+                                            f'Тема: {theme}\n'
+                                            f'Описание: {description}\n'
+                                            f'Статус: {status}')
     else:
-        order_number = max_order_number + 1
-    lines = text.split('\n')
-    theme = lines[0]
-    description = '\n'.join(lines[1:])
-    cursor.execute('INSERT INTO orders (user_name, theme, description, order_number) VALUES (?,?,?,?)',
-                   (user_name, theme, description, order_number))
-    conn.commit()
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f'Заказ с номером {order_number} не найден.')
+
     conn.close()
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text=f'Ваш заказ №{order_number} успешно отправлен!')
+    context.user_data['waiting_for_order_number'] = False  # Сбрасываем состояние ожидания номера заказа
 
 
-async def handle_photo_and_text(update, context):
+# Функция для обработки текстовых сообщений
+async def handle_message(update, context):
     ensure_table_exists()
-    user_name = update.message.from_user.username
-
-    # Инициализация переменной для хранения имени изображения
-    image_name = None
-
-    # Сохранение изображения
-    if update.message.photo:
-        photo_file = await update.message.photo[-1].get_file()
-        user_folder = os.path.join('orders', user_name)
-        os.makedirs(user_folder, exist_ok=True)
-        image_name = f'photo_{update.message.message_id}.jpg'
-        photo_path = os.path.join(user_folder, image_name)
-        await photo_file.download_to_drive(photo_path)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text='Изображение успешно сохранено!')
-
-    # Обработка текста, если он есть в сообщении
-    if update.message.caption:
-        text = update.message.caption
+    if context.user_data.get('waiting_for_order_number'):
+        await handle_order_number(update, context)
+    elif context.user_data.get('can_submit_order'):
+        text = update.message.text
+        user_name = update.message.from_user.username
         conn = sqlite3.connect('orders.db')
         cursor = conn.cursor()
-        cursor.execute('SELECT MAX(order_number) FROM orders')
-        max_order_number = cursor.fetchone()[0]
-        if max_order_number is None:
-            order_number = 1
-        else:
-            order_number = max_order_number + 1
+
+        # Генерация случайного 5-значного номера заказа
+        order_number = random.randint(10000, 99999)
+
+        # Разделение сообщения на тему и описание
         lines = text.split('\n')
         theme = lines[0]
         description = '\n'.join(lines[1:])
-        cursor.execute('INSERT INTO orders (user_name, theme, description, order_number, image) VALUES (?,?,?,?,?)',
-                       (user_name, theme, description, order_number, image_name))
+        cursor.execute('INSERT INTO orders (user_name, theme, description, order_number) VALUES (?,?,?,?)',
+                       (user_name, theme, description, order_number))
         conn.commit()
         conn.close()
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text=f'Ваш заказ №{order_number} успешно отправлен!')
+        context.user_data['can_submit_order'] = False  # Сбрасываем флаг разрешения отправки заказа
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text='Пожалуйста, нажмите "Оформить заказ", чтобы отправить заказ.')
 
 
-def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler('start', start))
-    app.add_handler(CallbackQueryHandler(button))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    app.add_handler(MessageHandler(filters.PHOTO | (filters.PHOTO & filters.TEXT), handle_photo_and_text))
-    app.run_polling()
+# Функция для обработки нажатий на кнопки
+async def button(update, context):
+    query = update.callback_query
+    await query.answer()
+    if query.data == "rules":
+        await rules(update, context)
+    elif query.data == "order":
+        await order(update, context)
+    elif query.data == "help":
+        await help(update, context)
+    elif query.data == "search":
+        context.user_data['waiting_for_order_number'] = False  # Убираем флаг ожидания номера заказа, если пользователь вернулся
+        await search_order(update, context)
+    elif query.data == "start":
+        context.user_data['waiting_for_order_number'] = False  # Убираем флаг ожидания номера заказа, если пользователь вернулся
+        await start(update, context)
 
 
+# Основная программа
 if __name__ == '__main__':
-    main()
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    # Обработчик команды /start
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    # Обработчик кнопок
+    button_handler = CallbackQueryHandler(button)
+    application.add_handler(button_handler)
+
+    # Обработчик текстовых сообщений
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message)
+    application.add_handler(message_handler)
+
+    # Запуск бота
+    application.run_polling()
